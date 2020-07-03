6792a9b21d0eeedaf2af04602bcdbbb0b2ba5700
Farah Khan <acufarah@Farahs-MacBook-Air.local>
2020-07-02 21:43:09 -0400

    Add image upload feature

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    public function show($user)
+    {
+        $user= User::findOrFail($user);
+        return view('profile.show', [
+            'user'=> $user,
+        ]);
+    }
+}
